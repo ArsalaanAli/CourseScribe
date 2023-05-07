ffc526e10f07b2e61fa17f084b5bb633da37a99b
Arsalaan Ali <arsalaanali2@gmail.com>
2023-05-07 17:01:06 -0400

    misc changes

---
+++ backend/testGPT.py
@@ -0,0 +1,4 @@
+import os
+import openai
+openai.organization = "org-RVYcd6jT0zJDkubEGsF3f48k"
+openai.api_key = os.getenv("OPENAI_API_KEY")
--- backend/testSR.py
+++ backend/testSR.py
@@ -3,10 +3,15 @@ import time
 
 r = sr.Recognizer()
 
-audio_file = sr.AudioFile('backend/EpicurusLecture.wav')
+audio_file = sr.AudioFile('backend/EpicurusLectureFull.wav')
+
+
+def printPartialTranscipt(transc):
+    print(transc)
+
 
 with audio_file as source:
-    audio_data = r.record(source)
+    audio_data = r.record(source, duration=None, offset=0)
 
 print("running")
 
@@ -19,4 +24,4 @@ time_elapsed = end_time - start_time
 with open('output_file.txt', 'w') as f:
     f.write(text)
 
-print("done in", time_elapsed)
+print("done in", time_elapsed, time_elapsed//60, time_elapsed % 60)
