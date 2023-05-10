d1ff3efbfbbaf99af8d25a461cd38c1bd278a48f
Arsalaan Ali <arsalaanali2@gmail.com>
2023-05-09 20:00:43 -0400

    gpt chunking and prompt testing

--- backend/gptTest.py
+++ backend/gptTest.py
@@ -4,31 +4,39 @@ import openai
 import os
 
 load_dotenv()
-api_key = os.getenv('API_KEY')
-db_name = os.getenv('DB_NAME')
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def runApiCall():
+def getNotes():
     with open('output_filefull.txt', 'r') as r:
+        notes = []
         try:
             transcription = r.read()
             chunks = get_chunks(transcription)
-            lecturePrompt = "This is a transcription of a lecture, write bullet points explaining the content of this lecture, make the bullet points thorough and make sure you cover every part of the lecture, start each bullet point with the symbol '•': \n" + \
-                chunks[3]
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {"role": "user", "content": lecturePrompt}
-                ]
-            )
-            return completion
+            lecturePrompt = "This is a transcription of a lecture, write bullet points explaining the content of this lecture, make the bullet points thorough and make sure you cover every part of the lecture, start each bullet point with the symbol '•': \n"
+            for c in chunks:
+                response = apiCall(lecturePrompt + c)
+                notes.append(response)
+                print("reponse recieved")
+
+            return notes
         except Exception as e:
             print(e)
-            return None
+            return notes
 
 
-response = runApiCall()
+def apiCall(prompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0
+    )
+    return completion["choices"][0]["message"]["content"]
 
-print(response)
+
+response = getNotes()
+print("="*20)
+for r in response:
+    print(r)
