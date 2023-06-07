72e956b55d1a9bf27918be1b041d620c0a263c6a
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2023-06-07 14:03:14 -0400

    setup prisma and planetscale

--- CourseScribe/src/pages/api/auth/[...nextauth].ts
+++ CourseScribe/src/pages/api/auth/[...nextauth].ts
@@ -1,22 +1,4 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import DiscordProvider from "next-auth/providers/discord";
+import { authOptions } from "~/server/auth";
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  /* 
-  TODO:
-    Find out what providers to use
-    Get client ID's for each provider
-    Setup redirect link for deployment
-    Change redirect URI in discord dev portal
-  */
-  providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
-};
 export default NextAuth(authOptions);
